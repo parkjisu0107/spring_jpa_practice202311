b495b5f72f95b0eacab1d4f8e64d948e637f9938
parkjisu0107 <parkjisu0107@gmail.com>
2023-11-23 10:00:47 +0900

    # 11. Swagger ui (API 명세서)

--- src/main/java/com/study/jpa/chap05_practice/api/PostApiController.java
+++ src/main/java/com/study/jpa/chap05_practice/api/PostApiController.java
@@ -6,8 +6,13 @@ import com.study.jpa.chap05_practice.service.PostService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -96,6 +101,12 @@ public class PostApiController {
 
 
 
+    @Operation(summary = "게시물 수정", description = "게시물 수정을 담당하는 메서드 입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "수정 완료!", content = @Content(schema = @Schema(implementation = PostDetailResponseDTO.class))),
+            @ApiResponse(responseCode = "400", description = "입력값 검증 실패"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND")
+    })
     // 게시물 수정
     @RequestMapping(method = {RequestMethod.PATCH, RequestMethod.PUT})
     public ResponseEntity<?> update(
